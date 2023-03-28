30a07578d5da7a39fc74610abec2e79b1b1b1d9e
Kaivmon <alex.svaikauskas@hotmail.com>
2023-03-27 20:09:15 -0400

    Added some comments to pseudocode Breathing and Quote Fetcher components

---
+++ Assets/Scripts/Breathe.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breathe : MonoBehaviour {
+
+    // This is just gonna be one of those simply breathing excersises for when you're feeling overwhelmed.
+    // an animated circle will expand, slow at first, fast in the middle, and then slow at the end.
+    // Same in reverse
+    // Text should appear below the circle for the first 3 inhales and exhales, saying breathe in, breathe out.
+        // Text should fade in at th estart, fade out at the end.
+    // Tap on the circle when you're done to dismiss.
+
+
+
+}
---
+++ Assets/Scripts/Quote Fetcher.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuoteFetcher : MonoBehaviour {
+    
+    // This script is to get a quote of the day from zenquotes.com
+    // Take the quote of the day and put it into the text field
+    // Maybe download like 50 quotes and keep them locally
+    // On button press, cycle through quotes that have already been shown
+    // If all quotes in the 50 have been shown then delete the last quotes, download 50 more and show the first one
+
+
+
+
+}
