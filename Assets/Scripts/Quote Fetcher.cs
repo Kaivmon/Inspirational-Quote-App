89689d15b9f7b579367640f1fbe41b0976e6d336
Kaivmon <alex.svaikauskas@hotmail.com>
2023-03-28 22:37:45 -0400

    Woking on adding text from a JSON request for the ZenQuotes API

--- Assets/Scripts/Quote Fetcher.cs
+++ Assets/Scripts/Quote Fetcher.cs
@@ -1,9 +1,43 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using SimpleJSON;
 using UnityEngine;
+using UnityEngine.Networking;
+using static System.Net.WebRequestMethods;
+using TMPro;
 
 public class QuoteFetcher : MonoBehaviour {
-    
+
+    public string url;
+
+    public JSONNode jsonResult;
+
+    public TextMeshPro quotetext;
+
+    // instance
+    public static QuoteFetcher instance;
+    void Awake() {
+        // set the instance to be this script
+        instance = this;
+    }
+
+    // sends an API request - returns a JSON file
+    private IEnumerator GetData(string location) {
+
+        // create the web request and download handler
+        UnityWebRequest webReq = new UnityWebRequest();
+        webReq.downloadHandler = new DownloadHandlerBuffer();
+        // build the url and query
+        webReq.url = "https://zenquotes.io/api/today";
+
+        quotetext.text = 
+
+        yield return null;
+
+    }
+
     // This script is to get a quote of the day from zenquotes.com
     // Take the quote of the day and put it into the text field
     // Maybe download like 50 quotes and keep them locally
