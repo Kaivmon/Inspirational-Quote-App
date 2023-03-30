fb3f032009bd848a51323931414d0d0e5814fd3c
Alex Svaikauskas <alex.svaikauskas@hotmail.com>
2023-03-30 17:32:25 -0400

    Working on Quote Fetcher.cs

--- Assets/Scripts/Quote Fetcher.cs
+++ Assets/Scripts/Quote Fetcher.cs
@@ -14,7 +14,10 @@ public class QuoteFetcher : MonoBehaviour {
 
     public JSONNode jsonResult;
 
-    public TextMeshPro quotetext;
+    public string quoteText;
+    public string quoteAuthor;
+
+    public Text quoteTextUI;  
 
     // instance
     public static QuoteFetcher instance;
@@ -29,15 +32,59 @@ public class QuoteFetcher : MonoBehaviour {
         // create the web request and download handler
         UnityWebRequest webReq = new UnityWebRequest();
         webReq.downloadHandler = new DownloadHandlerBuffer();
+
         // build the url and query
         webReq.url = "https://zenquotes.io/api/today";
 
-        quotetext.text = 
+        // Example return JSON from this call:
+        /*
+        
+        [ {
+           "q":"Spend eighty percent of your time focusing on the opportunities of tomorrow rather than the problems of yesterday.",
+           "a":"Brian Tracy",
+           "h":"<blockquote>&ldquo;Spend eighty percent of your time focusing on the opportunities of tomorrow rather than the problems of yesterday.&rdquo; &mdash; <footer>Brian Tracy</footer></blockquote>"
+        } ]
+        */
 
-        yield return null;
+        yield return webReq.SendWebRequest();
+
+        // convert the byte array and wait for a returning result
+        string rawJson = Encoding.Default.GetString(webReq.downloadHandler.data);
+
+        // parse the raw string into a json result we can easily read
+        jsonResult = JSON.Parse(rawJson);
+
+
+        // Testing result
+        Console.Log(jsonResult);
+
+        
+
+
+        // Using JSON
+        // Send a webrequest to receive the data from the api specified in the url
+        // Data should be returned as a JSONNode object, containing an array of key-value pairs
+        // This object can be saved as a large list of quotes
+            // We then need a function to loop through the array and find the next quote that hasn't been displayed. 
+            // Save the index number, move to the next index number when you need the next quote.
+            // Once the index reaches the last index in the array, make another webrequest to pull a new JSON object containing a new list of quotes
+        // To get specific data from the indexed object, zenquotes properties are:
+        // "q" key for quote
+        // "h" key for HTML pre-formatted quote
+        // "a" key for author
+        // "i" key for image
+        // "c" key for character count in the quote
+
+
+
+
+
 
     }
 
+
+    // OVERVIEW
+    // -------------------------------------------------------------------------------------------
     // This script is to get a quote of the day from zenquotes.com
     // Take the quote of the day and put it into the text field
     // Maybe download like 50 quotes and keep them locally
